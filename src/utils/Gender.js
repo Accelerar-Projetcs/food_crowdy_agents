b74f1c773b727ceaa8d7dd1ad1c6e6b08e7a34bb
cmcWebCode40 <infinity_michael@yahoo.com>
2020-09-09 09:54:20 +0100

    chore:modularized utils func

---
+++ src/utils/Gender.js
@@ -0,0 +1,25 @@
+export const gender = [
+	{
+		id: 1,
+		value: 'Male'
+	},
+	{
+		id: 2,
+		value: 'Female'
+	}
+];
+
+export const relationshipStatus = [
+	{
+		id: 1,
+		value: 'single'
+	},
+	{
+		id: 2,
+		value: 'married'
+	},
+	{
+		id: 3,
+		value: 'divorced'
+	}
+];
